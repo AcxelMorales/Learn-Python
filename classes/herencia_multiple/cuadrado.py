e18c1236fc99988ae1659cbdb78f749cce7c6462
Acxel Morales <acxelmorales97@gmail.com>
2020-06-08 15:23:35 -0500

    Herencia multiple

---
+++ classes/herencia_multiple/cuadrado.py
@@ -0,0 +1,12 @@
+from classes.herencia_multiple.color import Color
+from classes.herencia_multiple.figura_geo import FiguraGeometrica
+
+
+class Cuadrado(FiguraGeometrica, Color):
+    def __init__(self, lado, color):
+        self.lado = lado
+        FiguraGeometrica.__init__(self, lado, lado)
+        Color.__init__(self, color)
+
+    def area(self):
+        return self.lado * self.lado
---
+++ classes/herencia_multiple/test.py
@@ -0,0 +1,5 @@
+from classes.herencia_multiple.cuadrado import Cuadrado
+
+c = Cuadrado(6, "Verde")
+print(c.area())
+print(c.get_color())
